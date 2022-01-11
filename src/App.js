ffea82ed8b5206178c8bbd84e4f4f7eb6bbc9d73
Alfath Noverio <alfathnv@gmail.com>
2022-01-11 17:51:30 +0700

    Add firebase google sign in auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { auth } from "./firebase-config";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 function App() {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+  }
+
   return (
     <div className="App">
-      
+      <button onClick={signInWithGoogle}>click me</button>
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyATo8ICWdffBVFzpWqF2mql48Z5nWnQYPw",
+  authDomain: "assemblr-challenge.firebaseapp.com",
+  projectId: "assemblr-challenge",
+  storageBucket: "assemblr-challenge.appspot.com",
+  messagingSenderId: "560676152579",
+  appId: "1:560676152579:web:803120747247a601bd3536",
+  measurementId: "G-5LNRKQ7VQW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
