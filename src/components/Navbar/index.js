42c13aa7375ad0ebcd825f2655420b916fb9fc89
Alfath Noverio <alfathnv@gmail.com>
2022-01-12 11:13:46 +0700

    Add Pagination on Home page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './index.scss';
 import { Link } from "react-router-dom";
-
+import { signOutGoogle } from '../../google-auth';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
     <header>
       <div className='left-wrapper'>
@@ -15,6 +17,7 @@ function Navbar() {
         </Link>
       </div>
       
+      {/* <button onClick={() => signOutGoogle(navigate)}></button> */}
       <div className="right-wrapper">
       </div>
     </header>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import { ref, get, child, query, orderByChild, limitToLast } from "firebase/database";
-import { signOutGoogle } from '../../google-auth';
-import { useNavigate } from 'react-router-dom';
+import { ref, get, query, orderByChild } from "firebase/database";
 import { db } from "../../firebase-config";
+import ReactPaginate from 'react-paginate';
 import './index.scss'
 
 function Home() {
-  const navigate = useNavigate();
   const [data, setData] = useState(() => []);
 
   useEffect(() => {
@@ -21,19 +19,45 @@ function Home() {
       setData(movies);
     })
   }, [data])
+  
+  const [pageNumber, setPageNumber] = useState(0);
 
-  return (
-    <div className='home-wrapper'>
-      {/* <button onClick={() => signOutGoogle(navigate)}></button> */}
-      <h1 className='home-title'>Movie List</h1>
-      {data.map((movie) =>
+  const dataPerPage = 10;
+  const pagesVisited = pageNumber * dataPerPage;
+
+  const displayData = data
+  .slice(pagesVisited, pagesVisited + dataPerPage)
+  .map((movie) =>
         <div className='movie-card' key={movie.toString()}>
           <h2 className='title'>{movie.movie_title}</h2>
           <p className='date'>( {movie.date} )</p>
           <img className='image' src={movie.movie_image} alt={data[0].movie_title} />
           <p className='desc'>{movie.movie_description}</p>
         </div>
-      )}
+      )
+
+  const pageCount = Math.ceil(data.length/dataPerPage);
+
+  const changePage = ({selected}) => {
+    setPageNumber(selected)
+  }
+
+  return (
+    <div className='home-wrapper'>
+      
+      <h1 className='home-title'>Movie List</h1>
+      {displayData}
+      <ReactPaginate 
+        previousLabel={"Prev"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"pagination"}
+        previousLinkClassName={"previous-btn"}
+        nextLinkClassName={"next-btn"}
+        disabledClassName={"pagination-disable"}
+        activeClassName={"pagination-active"}
+      />
     </div>
   )
 }
